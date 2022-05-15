f6c8bcab3c4f701bf6138136d4d917c8a8d07bde
AndreyGamolya <gamolya2009@yandex.ru>
2022-05-15 17:33:52 +0300

    gamolya

---
+++ lesson 2.py
@@ -0,0 +1,112 @@
+
+st = 'Hello * how * are you?'
+st2 = 'Maria'
+
+# print(st2 +st)
+# print(st2 * 5)
+#
+# # print(st2[2]) # взятие эл-та по индексу
+# # print(st2[0])
+# # print(st[0])
+# #
+# # print(st[-1])
+# #
+# # last = st[-1]
+# # print(st[::-1])
+# #
+# # l = len(st) # кол-во символов в строке
+# # print(l)
+# #
+# # print(len(st2))
+# #
+# # print(st[l-1])
+# #
+# # st = 'Hello    how    are you?'
+# #
+# # print(st.split(''))
+# #
+# # st3 = input ()
+# # st3 = st3.split()
+# # print(st3)
+#
+# new = '-'.join(['hello', 'hi'])
+# print(new)
+#
+# print(st.replace('*', '____'))
+#
+# print(st.count('o')) # считает кол-во симоволов
+#
+# print(st.upper())
+#
+# # ans = input('продолжить? ').lower()
+# # if ans == 'да':
+# #     print('go')
+#
+# # списки
+#
+# print(list('Hello'))
+#
+# li = ['sd', 12, 65, True, [1, 2]]
+# print(li)
+#
+# print(li[3])
+# print(li[1:5])
+#
+# print(li[-1][0])
+#
+# li[0], li[-1] = li[-1], li[0]
+# print(li)
+#
+# # while - условный
+# # for - счетный
+#
+# # for element in li:
+# #     print(element)
+# #     if element == True:
+# #         break
+# li = ['sd', 12, 65, True, [1, 2]]
+#
+# # print(list(range(1,5)))
+# # print('*' * 12)
+# # for i in range(0, len(li)):
+# #     print(li[i])
+#
+# li = ['sd', 12, 65, True, [1, 2]]
+# li.append('123')
+# print(li)
+# li.insert(1, 12)
+# print(li)
+# li.count(12)
+# print(li.count(12))
+#
+# el = 12
+# if el in li:
+#     li.remove(el) #удаление по названию
+# print(li)
+#
+# li.pop(3) #удаление по индексу
+# print(li)
+#
+# for i, element in enumerate(li):
+#     print(i, element)
+#
+# cort = (12, 'true', 5.7, 12)
+# print(cort[0])
+# print(cort.count(12))
+
+# di = {'Ann': 123, 'Bob': 341, 'Mike': [986, 123, 866]}
+# print(di)
+# print(di['Bob'])
+# di['Kate'] = 761
+# print(di)
+#
+# print(di.keys())
+# print(di.values())
+
+# marks = {'otl': [10, 9, 8], 'chor': [7, 6], 'udv': [5,4], 'neud': [3, 2, 1]}
+# num = int(input('Num: '))
+# for el in marks:
+#     if num in marks[el]:
+#         print(el)
+#         break
+
---
+++ main.py
@@ -0,0 +1,81 @@
+# print('world')
+#
+# # int - целое число
+# age = 10
+# print(age)
+# print(12+9-30)
+# res = 5 * 7 - 9
+# n =  res + age * 5
+# print(n)
+#
+# print(34 % 4)
+#
+# sec = 861743
+# print(sec // 60 // 60)
+# print(sec // 3600)
+#
+# print(2 ** 3)
+# print(type(age))
+
+# float - десятичные числа
+# temp = 37.8
+# money = 12.865
+# print(type(temp))
+#
+# # str - строка
+# name = "Maria"
+# city = "MSC"
+# book = "book: 'war and peace'"
+#
+# print(name + city)
+# print(name*4)
+
+# name = input("name: ")
+# age = input("age: ")
+# print("Name:", name, ",age", age)
+#
+# num1 = input("1: ")
+# num1 = int(num1)
+# num2 = input("2: ")
+# num2 = int(num2)
+# print(num1 + num2)
+#
+# age = 90
+# if age == 9:
+#     print("проходите")
+# elif age == 8:
+#     print("вам подарок")
+# elif age != 90:
+#     print("не 90")
+# else:
+#     print('идите на фильм')
+
+# цикл while - условный
+
+#
+# correct = '123'
+# at = 3
+# passw = input('Password: ')
+# while passw != correct:
+#     print("error")
+#     passw = input('Password: ')
+#     at -= 1
+#     if at == 0:
+#         break
+# print('correct')
+#
+# num = 34565
+# res = 0
+# while num > 0:
+#     last = num % 10 # сохраняем последнюю цифру
+#     res += 1
+#     num = num // 10
+# print(res)
+
+# name = input("name: ")
+# age = input("age: ")
+# print(f'Имя: {name}, Возраст: {age}')
+
+
+
+
